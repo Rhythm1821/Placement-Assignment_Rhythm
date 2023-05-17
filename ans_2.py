6c6515bb2561d60f840262948c54e152df6864fc
Rhythm Rawat <106919276+Rhythm1821@users.noreply.github.com>
2023-05-17 17:08:07 +0530

    Delete ans_2.py

--- ans_2.py
+++
@@ -1,9 +0,0 @@
-inp = input().replace(' ','')
-# print(inp)
-frequency = {}
-for letter in inp:
-    frequency[letter]=0 if letter not in frequency else frequency[letter]
-    frequency[letter]+=1
-values_set = set(frequency.values())   
-print('Yes') if len(values_set)==1 else print('No')
-    
